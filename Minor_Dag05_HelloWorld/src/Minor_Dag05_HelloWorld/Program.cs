89ed5a37dd864515c6568bbd5c4a752410e53ffe
Pim van Gurp <PimG@minor.infosupport.edu>
2016-09-12 14:04:33 +0200

    Add hello world CLI application

---
+++ Minor_Dag05_HelloWorld/src/Minor_Dag05_HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minor_Dag05_HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
