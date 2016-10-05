ff54e150462a306d9fe96504a438793508a3afce
Pim van Gurp <PimG@minor.infosupport.edu>
2016-10-05 13:33:53 +0200

    wip

---
+++ MonumentBackend/MonumentBackend.Test/Controllers/MonumentControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonumentBackend.Test.Controllers
+{
+    [TestClass]
+    public class MonumentControllerTest
+    {
+        [TestMethod]
+        public void GetGivesAnEnumerableOfMonuments()
+        {
+            // Arrange
+            MonumentController controller = ProvideMonumentController();
+
+            // Act
+
+            // Assert
+        }
+
+        private MonumentController ProvideMonumentController()
+        {
+            IMonumentDbContext context = new MonumentContextMock();
+            IRepository repository = new MonumentRepositoryMock();
+            MonumentController controller = new MonumentController();
+
+            return controller;
+        }
+    }
+}
