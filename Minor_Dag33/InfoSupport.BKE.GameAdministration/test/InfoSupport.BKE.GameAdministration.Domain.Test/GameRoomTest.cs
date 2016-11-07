bf9ffdd85aed790f9f26fa431911e432991f6b9f
Pim van Gurp <pimgvangurp@gmail.com>
2016-11-07 16:24:35 +0100

    Setup microservice

---
+++ Minor_Dag33/InfoSupport.BKE.GameAdministration/test/InfoSupport.BKE.GameAdministration.Domain.Test/GameRoomTest.cs
@@ -0,0 +1,43 @@
+﻿using InfoSupport.BKE.GameAdministration.Domain.Enums;
+using InfoSupport.BKE.GameAdministration.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InfoSupport.BKE.GameAdministration.Domain.Test
+{
+    [TestClass]
+    public class GameRoomTest
+    {
+        [TestMethod]
+        public void GameRoomCanBeCreated()
+        {
+            // Arrange
+            Player player = Player.Register("Vaardigheden");
+
+            // Act
+            GameRoom room = GameRoom.Create("Chessmatch04", GameType.TicTacToe, player);
+
+            // Assert
+            Assert.AreEqual("Chessmatch04", room.Name);
+            Assert.AreEqual(GameType.TicTacToe, room.GameType);
+            CollectionAssert.Contains(room.Players, player);
+        }
+
+        [TestMethod]
+        public void GameRoomCanBeJoined()
+        {
+            // Arrange
+            Player initialPlayer = Player.Register("Vaardigheden");
+            GameRoom room = GameRoom.Create("Chessmatch04", GameType.TicTacToe, initialPlayer);
+            Player joiningPlayer = Player.Register("BestTicTacToePlayerEver");
+
+            // Act
+            room.Join(joiningPlayer);
+
+            // Assert
+            Assert.AreEqual("Chessmatch04", room.Name);
+            Assert.AreEqual(GameType.TicTacToe, room.GameType);
+            CollectionAssert.Contains(room.Players, initialPlayer);
+            CollectionAssert.Contains(room.Players, joiningPlayer);
+        }
+    }
+}
---
+++ Minor_Dag33/Microservice Solution Template/test/InfoSupport.BKE.GameAdministration.Domain.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfoSupport.BKE.GameAdministration.Domain.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
